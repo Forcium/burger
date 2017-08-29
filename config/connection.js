3cf05beebfad9f2e2db75f6c83ac2e1586cb4047
Eddie <itein@Hotmail.com>
2017-08-28 18:11:24 -0700

    server connection reattempt

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,7 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
